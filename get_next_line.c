2f14252182802892da6f017a30805158921a7eab
Sherry <50695343+yu-lin23@users.noreply.github.com>
2019-07-07 13:32:03 +0200

    Changed

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: yu-lin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/26 14:47:16 by yu-lin            #+#    #+#             */
-/*   Updated: 2019/07/05 23:52:49 by yu-lin           ###   ########.fr       */
+/*   Updated: 2019/07/07 13:31:14 by yu-lin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,53 +14,21 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-int		read_line(int fd, char **buffer, int buff_size)
-{
-	char *holder;
-	char tmp_buffer[BUFF_SIZE + 1];
-	ssize_t size;
-
-	size = read(fd, tmp_buffer, BUFF_SIZE);
-	tmp_buffer[size] = '\0';
-	holder = ft_strjoin(*buffer, tmp_buffer);
-	free(*buffer);
-	*buffer = ft_strdup(holder);
-	free(holder);
-	return (1);
-}
-
-int		from_where(char *str, char c)
-{
-	int i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == c)
-			return (i);
-		i++;
-	}
-	return (-1);
-}
-
 int		get_next_line(const int fd, char **line)
 {
 	static char *tmp_buffer = NULL;
+	char buffer[BUFF_SIZE + 1];
 	int error;
 
-	if (tmp_buffer == NULL)
+	if (!tmp_buffer)
 		tmp_buffer = (char*)ft_memalloc(sizeof(char));
-	while (ft_strchr(tmp_buffer, '\n') == NULL)
+	while ((red = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
-		error = read_line(fd, &tmp_buffer, BUFF_SIZE);
-		if (error == 0)
-			return (0);
-		else if (error == -1)
-			return (-1);
+		buffer[red] = '\0';
+		holder = ft_strjoin(holder, buffer);
+		if (holder == '\n')
+			break;
 	}
-	*line = ft_strsub(tmp_buffer, 0, from_where(tmp_buffer, '\n'));
-	tmp_buffer = ft_strchr(tmp_buffer, '\n');
-	tmp_buffer++;
 	return (1);
 }
 
--- input_output.c
+++ input_output.c
@@ -6,7 +6,7 @@
 /*   By: yu-lin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 08:27:12 by yu-lin            #+#    #+#             */
-/*   Updated: 2019/07/05 16:20:22 by yu-lin           ###   ########.fr       */
+/*   Updated: 2019/07/07 11:18:49 by yu-lin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,23 @@
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
+#include <string.h>
 
 #define BUFF_SIZE 8
 
 int main(void)
 {
+	char *holder;
 	char str[BUFF_SIZE + 1];
 	int fd;
 	int size;
 
 	fd = open("nyan.txt", O_RDONLY, 0);
-	size = read(fd, &str, BUFF_SIZE);
+	size = read(fd, str, BUFF_SIZE);
 	printf("fd = %d\n", fd);
 	printf("size = %d\n", size);
 	printf("str = %s\n", str);
 
+	//printf("while str = %s\n", str);
 	return (0);
 }
