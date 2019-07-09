9666031828a5fdc8d64c2273a8fc8923d94763c1
Sherry <50695343+yu-lin23@users.noreply.github.com>
2019-07-09 16:59:39 +0200

    It's working

--- get_next_line.c
+++ get_next_line.c
@@ -6,76 +6,95 @@
 /*   By: yu-lin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/26 14:47:16 by yu-lin            #+#    #+#             */
-/*   Updated: 2019/07/07 14:06:25 by yu-lin           ###   ########.fr       */
+/*   Updated: 2019/07/09 15:07:43 by yu-lin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include "get_next_line.h"
-#include <stdio.h>
 
-int		read_line(int fd, char **temp_buffer)
+int		read_line(int fd, char **tmp_buffer)
 {
-	char *holder;
-	char buffer[BUFF_SIZE + 1];
-	ssize_t size;
+	char	buffer[BUFF_SIZE + 1];
+	char 	*holder;
+	size_t size;
 
 	size = read(fd, buffer, BUFF_SIZE);
 	buffer[size] = '\0';
-	holder = ft_strjoin(*temp_buffer, buffer);
-	free(*temp_buffer);
-	*temp_buffer = ft_strdup(holder);
-	free(holder);
+	holder = ft_strjoin(tmp_buffer[fd], buffer);
+	ft_strdel(&tmp_buffer[fd]);
+	tmp_buffer[fd] = ft_strdup(holder);
+	ft_strdel(&holder);
 	return (size);
 }
 
-int		from_where(char *str, char c)
+int		from_where(char *str)
 {
 	int i;
 
 	i = 0;
-	while (str[i])
-	{
-		if (str[i] == c)
-			return (i);
+	while (str[i] != '\n' && str[i] != '\0')
 		i++;
+	return (i);
+}
+
+int		ft_putline(char **str, char **line, int fd, int result)
+{
+	char *placeholder;
+	size_t i;
+
+	i = from_where(str[fd]);
+	if (str[fd][i] == '\n')
+	{
+		*line = ft_strsub(str[fd], 0, i);
+		placeholder = ft_strdup(str[fd] + i + 1);
+		ft_strdel(&str[fd]);
+		str[fd] = placeholder;
+		if (str[fd][0] == '\0')
+			ft_strdel(&str[fd]);
 	}
-	return (-1);
+	else if (str[fd][i] == '\0')
+	{
+		if (result == BUFF_SIZE)
+			return (get_next_line(fd, line));
+		*line = ft_strdup(str[fd]);
+		ft_strdel(&str[fd]);
+	}
+	return (1);
 }
 
 int		get_next_line(const int fd, char **line)
 {
-	static char *tmp_buffer = NULL;
-	int error;
+	static char *tmp_buffer[1024];
+	int result;
 
-	if (tmp_buffer == NULL)
-		tmp_buffer = (char*)ft_memalloc(sizeof(char));
-	while (ft_strchr(tmp_buffer, '\n') == NULL)
+	if (tmp_buffer[fd] == NULL)
+		tmp_buffer[fd] = (char*)ft_memalloc(sizeof(char));
+	while (ft_strchr(tmp_buffer[fd], '\n') == NULL)
 	{
-		error = read_line(fd, &tmp_buffer);
-		if (error < 1)
-			return (error);
+		result = read_line(fd, tmp_buffer);
+		if (result < 0)
+			return (-1);
+		else if (result == 0 && (tmp_buffer[fd] == NULL || tmp_buffer[fd][0] == '\0'))
+			return (0);
 	}
-	*line = ft_strsub(tmp_buffer, 0, from_where(tmp_buffer, '\n'));
-	tmp_buffer = ft_strchr(tmp_buffer, '\n');
-	tmp_buffer++;
-	return (1);
+	return (ft_putline(tmp_buffer, line, fd, result));
 }
 
 int		main(int ac, char **av)
 {
 	int fd;
-	int i;
-	char *buffer;
+	char *buffer = (char *)malloc(sizeof(char) * 1024);
 
-	if (ac == 2)
-	{
+	if (ac == 1)
+		return (0);
+	else if (ac == 2)
 		fd = open(av[1], O_RDONLY);
-		i = get_next_line(fd, &buffer);
-		while (i > 0)
-		{
-			printf("%s\n", buffer);
-		}
+	while (get_next_line(fd, &buffer)	> 0)
+	{
+		ft_putendl(buffer);
+		free(buffer);
 	}
+	if (ac == 2)
+		close(fd);
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,18 @@
 /*   By: yu-lin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/26 16:45:48 by yu-lin            #+#    #+#             */
-/*   Updated: 2019/07/03 15:19:35 by yu-lin           ###   ########.fr       */
+/*   Updated: 2019/07/09 15:01:36 by yu-lin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "libft.h"
 #include <fcntl.h>
 
-#define BUFF_SIZE 42
+#define BUFF_SIZE 32
 
 int		get_next_line(const int fd, char **line);
+
 #endif
