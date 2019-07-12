05b3f7fa4fdd8d09a11e92770b12c233fc001301
Sherry <50695343+yu-lin23@users.noreply.github.com>
2019-07-12 18:03:54 +0200

    Stuff

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: yu-lin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/10 08:19:17 by yu-lin            #+#    #+#             */
-/*   Updated: 2019/07/10 10:36:21 by yu-lin           ###   ########.fr       */
+/*   Updated: 2019/07/12 17:57:39 by yu-lin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,37 @@ int		main(int ac, char **av)
 {
 	int		fd;
 	char	*buffer;
+	int		n;
+	int		return_value;
 
-	if (ac == 1)
+	fd = 0;
+	n = 0;
+	return_value = 0;
+	if (ac < 2)
+	{
+		ft_putendl("Too few arguments, dude!");
 		return (0);
+	}
 	else if (ac == 2)
 		fd = open(av[1], O_RDONLY);
-	while (get_next_line(fd, &buffer) > 0)
+	if (fd == -1)
 	{
+		ft_putendl("Uhmmm...file not found, bruh");
+		return (0);
+	}
+	while ((return_value = get_next_line(fd, &buffer)) > 0)
+	{
+		ft_putchar('[');
+		ft_putnbr(return_value);
+		ft_putstr("]-> Line[");
+		ft_putnbr(n);
+		ft_putstr("]	");
 		ft_putendl(buffer);
 		free(buffer);
+		n++;
 	}
-	if (ac == 2)
-		close(fd);
+	if (return_value == 0)
+		ft_putstr("**********End Of File**********");
+	close(fd);
 	return (0);
 }
