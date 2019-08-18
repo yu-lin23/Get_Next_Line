61dd87a37a58cf9cc3cbaabef607ee059dce73ec
Sherry <50695343+yu-lin42@users.noreply.github.com>
2019-08-18 10:31:27 +0200

    Delete gnl.sh

--- gnl.sh
+++
@@ -1,4 +0,0 @@
-gcc -Wall -Werror -Wextra get_next_line.c main.c libft/libft.a
-#gcc -Wall -Werror -Wextra get_next_line.c main_stenner.c libft/libft.a
-read -p 'What file do you want to run with GNL? :' filename
-./a.out ${filename}
